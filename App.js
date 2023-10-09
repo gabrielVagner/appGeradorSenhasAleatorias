201b1586818a629f86f7fdea7a126ff902ae314c
gabriel <gabrielvagner0701@gmail.com>
2023-10-09 17:13:43 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Slider from '@react-native-community/slider'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/cadeado.png')}
+        style={styles.img}
+      />
+      <Text style={styles.text}>16 Caracteres</Text>
+
+
+      <View style={styles.viewSlider}>
+        <Slider
+        style={{height: 70}}
+        minimumValue={6}
+        maximumValue={20}
+        />
+      </View>
+    
     </View>
   );
 }
@@ -13,8 +27,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#bbb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'black',
+    fontSize: 25
+    
+  },
+  img: {
+    width: 120,
+    height: 120,
+    marginTop: '10%',
+    marginBottom:'10%'
+  },
+  viewSlider:{
+    width: '75%'
+
+  }
 });
